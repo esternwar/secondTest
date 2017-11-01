8e2506edc988a66f84d9aa00b75dce605b714953
esternwar <saska77@mail.ru>
2017-11-01 20:34:00 +0300

    1
    123

--- new_fold/new_line.py
+++ new_fold/new_line.py
@@ -130,7 +130,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url="https://bot.herokuapp.com/new_line")
+    bot.set_webhook(url="https://botniceref.herokuapp.com/new_line")
     return "!", 200
 
 server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
